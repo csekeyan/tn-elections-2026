a5f7c34620b7662450dc431e1c29745c30903c61
Karthikeyan Gopal <20188684+csekeyan@users.noreply.github.com>
2026-05-01 23:58:27 -0700

    Add plugins array to vite config for Cloudflare Wrangler

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
   base: '/tn-elections-2026/',
+  plugins: [],
   build: {
     outDir: '../dist',
     emptyOutDir: true,
